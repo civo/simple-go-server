0e5b12b5b66e2067baecfcce5cabb6d17f5b005c
alejandrojnm <alejandrojnm@gmail.com>
2023-02-01 12:35:46 +0000

    Update the project

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"embed"
-        "html/template"
+    "html/template"
 	"fmt"
 	"log"
 	"net/http"
@@ -17,21 +17,26 @@ func main() {
 	if port == "" {
 		port = "8080"
 	}
-        t, err := template.ParseFS(static, "static/demo.html")
+    
+	// Create a new serve mux and register the static file server.
+	mux := http.NewServeMux()
+
+	t, err := template.ParseFS(static, "static/demo.html")
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Println("Starting server on port", port)
-        var staticFS = http.FS(static)
+	
+	var staticFS = http.FS(static)
 	fs := http.FileServer(staticFS)
 
 	// Serve static files
-	http.Handle("/static/", fs)
-	mux := http.NewServeMux()
+	mux.Handle("/static/", fs)
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add("Content-Type", "text/html")
                 t.Execute(w,"" )
 	})
+	
+	log.Println("Starting server on port", port)
 	err = http.ListenAndServe(fmt.Sprintf(":%s", port), mux)
 	if err != nil {
 		log.Fatal(err)
